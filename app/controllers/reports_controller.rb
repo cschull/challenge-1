753956406442cf2b718f03981f975e7a9c92e9df
Camille Schull <camille.schull@gmail.com>
2020-12-28 10:42:18 -0500

    done

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,19 +1,29 @@
-
+require 'csv'
+require 'byebug'
 
 class ReportsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_server
 
   def handle
-    render json: { message: 'testing' }
     report = params["reports.csv"].open
-    csv_options = { col_sep: ",", headers: first_row }
-    CSV.parse( reportm csv_options ) do |timestamp, lock_id, kind, status|
-      p timestamp[1]
-      p lock_id[1]
-      p kind[1]
-      p status[1]
+    p report
+    csv_options = { col_sep: ",", headers: true }
+    CSV.parse( report, csv_options ) do |timestamp, lock_id, kind, status|
+      timestamp = timestamp
+      lock_id = lock_id
+      kind = kind
+      status_change = status
+      lock = Lock.find_by_id(lock_id)
+      if lock
+        lock.status = status_change
+      else
+        lock = Lock.create(id: lock_id, kind: kind, status: status_change)
+      end
+      Entry.create(timestamp: timestamp, status_change: status_change, lock: lock)
     end
+
+    render json: { message: 'Your report has been processed. You now have #{Lock.count} lock and #{Entry.count} entries.' }
   end
 
   def authenticate_server
